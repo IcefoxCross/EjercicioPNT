bcecc8ba6890b3dfe54c4ad786a68bc6bf4dfa32
IcefoxCross <icefox.990@gmail.com>
2019-03-03 20:06:28 -0300

    clases de producto

--- Comercio.java
+++ Comercio.java
@@ -1,8 +1,8 @@
-class Producto {
+class Producto implements Comparable {
     private String nombre;
-    private float precio;
+    private double precio;
 
-    public Producto(String _nombre, float _precio) {
+    public Producto(String _nombre, double _precio) {
         nombre = _nombre;
         precio = _precio;
     }
@@ -11,12 +11,67 @@ class Producto {
         return nombre;
     }
 
-    public float getPrecio() {
+    public double getPrecio() {
         return precio;
     }
 
     public String toString() {
-        return "Nombre: "+nombre+" /// Precio: $"+Math.round(precio);
+        return String.format("Nombre: %1s /// Precio: $%2s", nombre,Math.round(precio));
+    }
+
+    public int compareTo(Producto p) {
+        return Double.compare(this.precio, p.precio);
+    }
+}
+
+class Bebida extends Producto {
+    private double litros;
+
+    public Bebida(String _nombre, double _litros, double _precio) {
+        super(_nombre, _precio);
+        litros = _litros;
+    }
+
+    public double getLitros() {
+        return litros;
+    }
+
+    public String toString() {
+        return String.format("Nombre: %1s /// Litros: %2s /// Precio: $%3s", getNombre(),litros,Math.round(getPrecio()));
+    }
+}
+
+class Limpieza extends Producto {
+    private String contenido;
+
+    public Limpieza(String _nombre, String _contenido, double _precio) {
+        super(_nombre, _precio);
+        contenido = _contenido;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public String toString() {
+        return String.format("Nombre: %1s /// Contenido: %2s /// Precio: $%3s", getNombre(),contenido,Math.round(getPrecio()));
+    }
+}
+
+class Fresco extends Producto {
+    private String uVenta;
+
+    public Fresco(String _nombre, String _uVenta, double _precio) {
+        super(_nombre, _precio);
+        uVenta = _uVenta;
+    }
+
+    public getUVenta() {
+        return uVenta;
+    }
+
+    public String toString() {
+        return String.format("Nombre: %1s /// Precio: %2s /// Unidad de Venta: $%3s", getNombre(),Math.round(getPrecio()),uVenta);
     }
 }
 
@@ -24,12 +79,15 @@ public class Comercio {
     public static void main(String[] args) {
         Producto[] p = cargarProductos();
 
-        System.out.println(p[0]);
+        for (int i=0;i<p.length;i++) {
+            System.out.println(p[i]);
+        }
     }
 
     public static Producto[] cargarProductos() {
         Producto[] p = new Producto[]{
-            new Producto("Frutillas", 64)
+            new Bebida("Coca-Cola Zero", 1.5, 20),
+            new Bebida("Coca-Cola",1.5,18),
         };
         return p;
     }
